9a23d387ac7755b8f67329cfb0b10314f9ec915d
Jonathan Avinor <javinor@gmail.com>
2020-12-05 21:58:31 +0200

    implement cipher

--- ch09/Cipher.hs
+++ ch09/Cipher.hs
@@ -1,4 +1,15 @@
-module Ch09_12_Exercises where
+module Cipher where
 
 import Data.Char
 
+shiftAlpha :: Int -> Char -> Char
+shiftAlpha n c = chr $ ((ord c - base + n) `mod` cycle) + base
+  where
+    cycle = ord 'z' - ord 'a' + 1
+    base = if isUpper c then ord 'A' else ord 'a'
+
+caesar :: Int -> String -> String
+caesar n xs = fmap (shiftAlpha n) xs
+
+unCaesar :: Int -> String -> String
+unCaesar n xs = fmap (shiftAlpha n) xs
